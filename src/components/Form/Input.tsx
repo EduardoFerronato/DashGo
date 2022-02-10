e2b1eb431e5dc57c577a585b82bc5e4f7ca249af
Eduardo Ferronato <edu.fe.guin@gmail.com>
2022-02-09 21:49:24 -0300

    entrar sem login

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,14 +1,16 @@
 import { forwardRef, ForwardRefRenderFunction } from "react";
-import { FormLabel, FormControl, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { FormLabel, FormControl, Input as ChakraInput, InputProps as ChakraInputProps, FormErrorMessage } from "@chakra-ui/react";
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, ...rest }, ref) => {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, error = null, ...rest }, ref) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
       <ChakraInput
         name={name}
@@ -23,6 +25,10 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ nam
         size="lg"
         {...rest}
       />
+
+      {!!error && (
+        <FormErrorMessage>{error.message}</FormErrorMessage>
+      )}
     </FormControl>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,36 @@
-import { Flex, Button, Stack } from "@chakra-ui/react"
+import { Flex, Button, Stack, Divider, Link as LinkChakra } from "@chakra-ui/react"
 import { Input } from "../components/Form/Input"
+import * as yup from "yup"
+import { yupResolver } from "@hookform/resolvers/yup"
 import { SubmitHandler, useForm } from "react-hook-form"
+import { Logo } from "../components/Header/Logo"
+import Link from "next/link"
 
 type SignInFormData = {
   email: string
   password: string
 }
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+  password: yup.string().required("Senha obrigatória"),
+})
+
 export default function SignIn() {
-  const { register, handleSubmit, formState } = useForm()
+  const { register, handleSubmit, formState: { errors }, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  // const toast = useToast()
+
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    // if (!values.email) {
+    //   toast({title:"Preencha um e-mail!", duration:2000})
+    // }
+
     await new Promise(resolve => setTimeout(resolve, 2000))
+
 
     console.log(values);
 
@@ -23,7 +42,9 @@ export default function SignIn() {
       h="100vh"
       align="center"
       justify="center"
+      flexDirection="column"
     >
+      <Logo />
       <Flex
         as="form"
         width="100%" //ou w=""
@@ -32,12 +53,25 @@ export default function SignIn() {
         padding="8" //ou p=""
         borderRadius={8}
         flexDir="column" //ou flexDirection=""
+        mt="4"
         onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
 
-          <Input name="email" type="email" label="E-mail" {...register("email")} />
-          <Input name="password" type="password" label="Senha" {...register("password")} />
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+            error={errors.email}
+            {...register}
+          />
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+            {...register}
+            error={errors.password}
+          />
 
         </Stack>
         <Button
@@ -50,6 +84,13 @@ export default function SignIn() {
           Entrar
         </Button>
       </Flex>
+      <Divider
+        w={300}
+        my="4"
+      />
+      <Link href="/dashboard" passHref>
+        <LinkChakra>Testar sem Login</LinkChakra>
+      </Link>
     </Flex>
   )
 }
